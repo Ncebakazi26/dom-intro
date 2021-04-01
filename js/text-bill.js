b6014481a6d132c690bb5725deeb5b0471696639
Ncebakazi <ncebakazi414@gmail.com>
2021-04-01 16:36:27 +0200

    made changes

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,11 +1,43 @@
+var callsTotalElem = document.querySelector(".callTotalOne")
+var smsTotalElem = document.querySelector(".smsTotalOne")
+var billTypeText = document.querySelector(".billTypeText")
+var totalCostElem = document.querySelector(".totalOne")
 // get a reference to the textbox where the bill type is to be entered
 
 //get a reference to the add button
-
+var addToBillBtn = document.querySelector(".addToBillBtn")
 //create a variable that will keep track of the total bill
-
+var callsTotal = 0;
+var smsTotal = 0;
 //add an event listener for when the add button is pressed
+function textBillTotal(){
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value.trim();
+    console.log(billTypeEntered)
+    // update the correct total
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
 
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElem.classList.add("warning");
+    }
+}
+
+addToBillBtn.addEventListener('click', textBillTotal);
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
